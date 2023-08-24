5b8dcb1510f035b8b41dbb40ec121e3af92b86b1
kipkoechke <chepkwonyke1@gmail.com>
2023-08-24 11:31:02 -0700

    The rotr opcode

--- monty_functions_4.c
+++ monty_functions_4.c
@@ -25,3 +25,29 @@ void monty_rotl(stack_t **stack, unsigned int line_number)
 
 	(void)line_number;
 }
+
+/**
+ * monty_rotr - Rotates the bottom value of a stack_t linked list to the top.
+ * @stack: A pointer to the top node of a stack_t linked list.
+ * @line_number: The current line number in the Monty bytecodes file.
+ */
+void monty_rotr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *top, *bottom;
+
+	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
+		return;
+
+	top = (*stack)->next;
+	bottom = (*stack)->next;
+	while (bottom->next != NULL)
+		bottom = bottom->next;
+
+	bottom->prev->next = NULL;
+	(*stack)->next = bottom;
+	bottom->prev = *stack;
+	bottom->next = top;
+	top->prev = bottom;
+
+	(void)line_number;
+}
