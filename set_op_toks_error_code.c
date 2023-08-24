ac851d7ffce6ed7889124601430a6826a63b0748
kipkoechke <chepkwonyke1@gmail.com>
2023-08-24 11:46:20 -0700

    Updated op_toks with an error code

---
+++ set_op_toks_error_code.c
@@ -0,0 +1,43 @@
+#include "monty.h"
+
+/**
+ * set_op_tok_error_code - Updates op_toks with an error code.
+ * @error_code: Integer error code to be stored as a string in op_toks.
+ */
+void set_op_tok_error_code(int error_code)
+{
+	int toks_len = 0, i = 0;
+	char *exit_str = NULL;
+	char **new_toks = NULL;
+
+	toks_len = token_arr_len();
+	new_toks = malloc(sizeof(char *) * (toks_len + 2));
+
+	if (!op_toks)
+	{
+		malloc_error();
+		return;
+	}
+
+	while (i < toks_len)
+	{
+		new_toks[i] = op_toks[i];
+		i++;
+	}
+
+	exit_str = get_int(error_code);
+
+	if (!exit_str)
+	{
+		free(new_toks);
+		malloc_error();
+		return;
+	}
+
+	new_toks[i++] = exit_str;
+	new_toks[i] = NULL;
+
+	free(op_toks);
+	op_toks = new_toks;
+}
+
