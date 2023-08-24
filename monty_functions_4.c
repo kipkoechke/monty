15d9ecb69bc99cd0679b49bc4ee05827ee12268c
kipkoechke <chepkwonyke1@gmail.com>
2023-08-24 11:35:29 -0700

    The queue opcode

--- monty_functions_4.c
+++ monty_functions_4.c
@@ -62,3 +62,15 @@ void monty_stack(stack_t **stack, unsigned int line_number)
 	(*stack)->n = STACK;
 	(void)line_number;
 }
+
+/**
+ * monty_queue - Converts a stack to a queue.
+ * @stack: A pointer to the top node of a stack_t linked list.
+ * @line_number: The current line number in the Monty bytecodes file.
+ */
+void monty_queue(stack_t **stack, unsigned int line_number)
+{
+	(*stack)->n = QUEUE;
+	(void)line_number;
+}
+
