deae3f4bd8076035d34d6614be28f31512457875
kipkoechke <chepkwonyke1@gmail.com>
2023-08-24 11:12:36 -0700

    The add opcode

---
+++ monty_functions_2.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+
+/**
+ * monty_add - Adds the top two values of a stack_t linked list.
+ * @stack: A pointer to the top mode node of a stack_t linked list.
+ * @line_number: The current working line number of a Monty bytecodes file.
+ *
+ * Description: The function adds the second value from the top to the
+ *              top value in the stack. The result is stored in the second
+ *              value node, and the top value is removed.
+ */
+void monty_add(stack_t **stack, unsigned int line_number)
+{
+	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
+	{
+		set_op_tok_error_code(short_stack_error(line_number, "add"));
+		return;
+	}
+
+	(*stack)->next->next->n += (*stack)->next->n;
+	monty_pop(stack, line_number);
+}
+
