b0dd4906e3952d7c7dfe1c4713a73372320d973e
kipkoechke <chepkwonyke1@gmail.com>
2023-08-24 11:17:09 -0700

    The mul opcode

--- monty_functions_2.c
+++ monty_functions_2.c
@@ -70,3 +70,25 @@ void monty_div(stack_t **stack, unsigned int line_number)
 	(*stack)->next->next->n /= (*stack)->next->n;
 	monty_pop(stack, line_number);
 }
+
+/**
+ * monty_mul - Multiplies the second value from the top of
+ *             a stack_t linked list by the top value.
+ * @stack: A pointer to the top mode node of a stack_t linked list.
+ * @line_number: The current working line number of a Monty bytecodes file.
+ *
+ * Description: The function multiplies the second value from the top of the stack
+ *              by the top value. The result is stored in the second value node,
+ *              and the top value is removed.
+ */
+void monty_mul(stack_t **stack, unsigned int line_number)
+{
+	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
+	{
+		set_op_tok_error_code(short_stack_error(line_number, "mul"));
+		return;
+	}
+
+	(*stack)->next->next->n *= (*stack)->next->n;
+	monty_pop(stack, line_number);
+}
