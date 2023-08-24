999d7ff6e638c9625a030c8ff28c8926fff0f18b
kipkoechke <chepkwonyke1@gmail.com>
2023-08-24 11:15:23 -0700

    The div opcode

--- monty_functions_2.c
+++ monty_functions_2.c
@@ -43,3 +43,30 @@ void monty_sub(stack_t **stack, unsigned int line_number)
 	monty_pop(stack, line_number);
 }
 
+/**
+ * monty_div - Divides the second value from the top of
+ *             a stack_t linked list by the top value.
+ * @stack: A pointer to the top mode node of a stack_t linked list.
+ * @line_number: The current working line number of a Monty bytecodes file.
+ *
+ * Description: The function divides the second value from the top of the stack
+ *              by the top value. The result is stored in the second value node,
+ *              and the top value is removed. If the top value is 0, an error is set.
+ */
+void monty_div(stack_t **stack, unsigned int line_number)
+{
+	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
+	{
+		set_op_tok_error_code(short_stack_error(line_number, "div"));
+		return;
+	}
+
+	if ((*stack)->next->n == 0)
+	{
+		set_op_tok_error_code(div_error(line_number));
+		return;
+	}
+
+	(*stack)->next->next->n /= (*stack)->next->n;
+	monty_pop(stack, line_number);
+}
