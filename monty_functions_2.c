87919446da587e776ba72541f98594d49df760b4
kipkoechke <chepkwonyke1@gmail.com>
2023-08-24 11:18:06 -0700

    The mod opcode

--- monty_functions_2.c
+++ monty_functions_2.c
@@ -92,3 +92,32 @@ void monty_mul(stack_t **stack, unsigned int line_number)
 	(*stack)->next->next->n *= (*stack)->next->n;
 	monty_pop(stack, line_number);
 }
+
+/**
+ * monty_mod - Computes the modulus of the second value from the
+ *             top of a stack_t linked list  by the top value.
+ * @stack: A pointer to the top mode node of a stack_t linked list.
+ * @line_number: The current working line number of a Monty bytecodes file.
+ *
+ * Description: The function computes the modulus of the second value from the
+ *              top of the stack by the top value. The result is stored in the
+ *              second value node, and the top value is removed. If the top value
+ *              is 0, an error is set.
+ */
+void monty_mod(stack_t **stack, unsigned int line_number)
+{
+	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
+	{
+		set_op_tok_error_code(short_stack_error(line_number, "mod"));
+		return;
+	}
+
+	if ((*stack)->next->n == 0)
+	{
+		set_op_tok_error_code(div_error(line_number));
+		return;
+	}
+
+	(*stack)->next->next->n %= (*stack)->next->n;
+	monty_pop(stack, line_number);
+}
