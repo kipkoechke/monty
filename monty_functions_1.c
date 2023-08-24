e7b22c06ece34657b8830273f68471ea642793cb
kipkoechke <chepkwonyke1@gmail.com>
2023-08-24 11:09:02 -0700

    The pop opcode

--- monty_functions_1.c
+++ monty_functions_1.c
@@ -87,3 +87,26 @@ void monty_pint(stack_t **stack, unsigned int line_number)
 
 	printf("%d\n", (*stack)->next->n);
 }
+
+/**
+ * monty_pop - Removes the top value element of a stack_t linked list.
+ * @stack: A pointer to the top node of a stack_t linked list.
+ * @line_number: The current line number in the Monty bytecodes file.
+ */
+void monty_pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *next = NULL;
+
+	if ((*stack)->next == NULL)
+	{
+		set_op_tok_error_code(pop_error(line_number));
+		return;
+	}
+
+	next = (*stack)->next->next;
+	free((*stack)->next);
+	if (next)
+		next->prev = *stack;
+	(*stack)->next = next;
+}
+
