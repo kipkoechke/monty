dca57f705de46f8cd2a28d8829573e283490193b
kipkoechke <chepkwonyke1@gmail.com>
2023-08-24 11:07:35 -0700

    The pint opcode

--- monty_functions_1.c
+++ monty_functions_1.c
@@ -71,3 +71,19 @@ void monty_pall(stack_t **stack, unsigned int line_number)
 	}
 	(void)line_number;
 }
+
+/**
+ * monty_pint - Prints the top value of a stack_t linked list.
+ * @stack: A pointer to the top node of a stack_t linked list.
+ * @line_number: The current line number in the Monty bytecodes file.
+ */
+void monty_pint(stack_t **stack, unsigned int line_number)
+{
+	if ((*stack)->next == NULL)
+	{
+		set_op_tok_error_code(pint_error(line_number));
+		return;
+	}
+
+	printf("%d\n", (*stack)->next->n);
+}
